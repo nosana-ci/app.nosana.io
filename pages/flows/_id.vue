d52ecd9d38da29543c296a51c31d46fe468f99b4
Laurens Verspeek <l.verspeek@gmail.com>
2021-12-06 16:55:47 +0100

    add flow page

---
+++ pages/flows/_id.vue
@@ -0,0 +1,115 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div v-if="flow">
+        <div class="is-flex is-align-items-center">
+          <div class="mr-4">
+            <nuxt-link to="/" class="has-text-secondary is-size-5">
+              <i class="fas fa-chevron-left" />
+            </nuxt-link>
+          </div>
+          <div class="mr-4">
+            <span v-if="curFlow === null" class="tag">loading</span>
+            <span v-else-if="flow.id === curFlow.id" class="tag is-info">pending</span>
+            <span v-else class="tag is-success">passed</span>
+          </div>
+          <div>Flow <b>#{{ flow.id }}</b> triggered {{ $moment(flow.results.input.commit.committer.date).fromNow() }}</div>
+        </div>
+        <hr class="my-4">
+        <h1 class="title">
+          {{ flow.results.input.commit.message.split('\n')[0] }}
+        </h1>
+        <div class="box">
+          <div>Pipeline total cost <b class="has-text-secondary">42.13 NOS<b /></b></div>
+          <hr>
+          <div>Nodes participated: <b>1</b></div>
+          <hr>
+          <div class="has-overflow-ellipses">
+            Commit <a :href="flow.results.input.html_url" target="_blank" @click.stop>{{ flow.results.input.sha }}</a>
+          </div>
+          <span style="white-space: pre-wrap">{{ flow.results.input.commit.message }}</span>
+        </div>
+        <div class="tabs is-medium">
+          <ul>
+            <li :class="{'is-active': tab === 'steps'}">
+              <a @click.prevent="tab='steps'">Steps</a>
+            </li>
+            <li :class="{'is-active': tab === 'logs'}">
+              <a @click.prevent="tab='logs'">Logs</a>
+            </li>
+            <li :class="{'is-active': tab === 'network'}">
+              <a @click.prevent="tab='network'">Network</a>
+            </li>
+          </ul>
+        </div>
+        <div v-if="tab === 'steps'">
+          <div v-for="op in flow.ops" :key="op.id" class="box is-info">
+            <div class="is-clickable is-flex is-flex-wrap-wrap is-align-items-center" @click="step !== op.id ? step = op.id : step = null">
+              <h3 class="subtitle m-0">
+                {{ op.title }}
+              </h3>
+              <div class="is-size-7 has-overflow-ellipses mr-4" style="margin-left: auto">
+                was ran by <a :href="`https://explorer.solana.com/address/${'7E5nsBVPuPoRBsDzVjr2YKFNoqewo2EGitKq7cbhSSp4'}`" target="_blank">7E5nsBVPuPoRBsDzVjr2YKFNoqewo2EGitKq7cbhSSp4</a>
+              </div>
+              <div>
+                <i class="fas fa-chevron-down" :class="{'fa-chevron-down': step !== op.id, 'fa-chevron-up': step === op.id}" />
+              </div>
+            </div>
+            <div v-if="step === op.id">
+              <div v-if="op.op === 'nos.git/ensure-repo'">
+                <pre>Cloning into {{ flow.results[op.id] }}</pre>
+              </div>
+              <div v-else-if="op.op === 'sh'">
+                <pre v-if="flow.results[op.id]">{{ flow.results[op.id].out }}</pre>
+                <div v-else>
+                  Op is still pending
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-else-if="tab === 'logs'">
+          {{ flow }}
+        </div>
+        <div v-else>
+          Coming soon..
+        </div>
+      </div>
+      <div v-else>
+        Loading..
+      </div>
+    </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      flow: null,
+      step: null,
+      curFlow: null,
+      tab: 'steps'
+    }
+  },
+  created () {
+    this.getFlow(this.$route.params.id)
+    this.getCurrentFlow()
+  },
+  methods: {
+    async getFlow (id) {
+      const flow = await this.$axios.$get(`${process.env.backendUrl}/api/flow/${id}`)
+      this.flow = flow
+    },
+    async getCurrentFlow () {
+      const flow = await this.$axios.$get(`${process.env.backendUrl}/api/cur-flow`)
+      this.curFlow = flow
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="flow in flows" :key="flow.id">
+            <tr v-for="flow in flows" :key="flow.id" class="is-clickable" @click="$router.push('/flows/'+flow.id)">
               <td>
                 <span v-if="curFlow === null" class="tag">loading</span>
                 <span v-else-if="flow.id === curFlow.id" class="tag is-info">pending</span>
@@ -23,7 +23,7 @@
               </td>
               <td>{{ flow.title }}</td>
               <td>{{ flow.results.input.commit.message.split('\n')[0] }}</td>
-              <td><a :href="flow.results.input.html_url" target="_blank">{{ flow.results.input.sha.substring(0,7) }}</a></td>
+              <td><a :href="flow.results.input.html_url" target="_blank" @click.stop>{{ flow.results.input.sha.substring(0,7) }}</a></td>
             </tr>
             <tr v-if="!flows || !flows.length" class="has-text-centered has-text-weight-bold">
               <td v-if="!flows" colspan="4">
@@ -59,7 +59,6 @@ export default {
     },
     async getCurrentFlow () {
       const flow = await this.$axios.$get(`${process.env.backendUrl}/api/cur-flow`)
-      console.log('flow', flow)
       this.curFlow = flow
     }
   }
