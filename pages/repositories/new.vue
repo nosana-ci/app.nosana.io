9eae4eb579c6ce33fc327cafbacb46837a855ed1
Laurens Verspeek <l.verspeek@gmail.com>
2022-02-04 16:44:49 +0100

    list repos of user

--- pages/repositories/new.vue
+++ pages/repositories/new.vue
@@ -7,9 +7,12 @@
       <h1 class="title is-4">
         Add new <b class="has-text-accent">Repository</b>
       </h1>
-      <a href="https://github.com/login/oauth/authorize?client_id=382b493152debb760a28&scope=repo">
+      <a href="https://github.com/login/oauth/authorize?client_id=382b493152debb760a28&scope=public_repo">
         Connect to Github
       </a>
+      <div v-for="repo in repositories" :key="repo.id">
+        {{ repo.full_name }}
+      </div>
       <form @submit.prevent="addRepository">
         <input v-model="repository" class="input" type="text" required>
         <button type="submit" class="button is-accent mt-2">
@@ -21,11 +24,14 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
       repository: null,
-      githubToken: null
+      githubToken: null,
+      repositories: null
     }
   },
   created () {
@@ -42,6 +48,15 @@ export default {
         code
       })
       this.githubToken = response.access_token
+      this.getUserRepos()
+    },
+    async getUserRepos () {
+      const githubApi = axios.create({
+        baseURL: 'https://api.github.com',
+        headers: { Authorization: 'token ' + this.githubToken }
+      })
+      const response = await githubApi.get('/user/repos?per_page=100')
+      this.repositories = response.data
     },
     async addRepository () {
       await this.$axios.$post(`${process.env.backendUrl}/repositories`, {
