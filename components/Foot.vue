ab0c028feb292334d61141d5ac3c7dc0b90c4f4f
Laurens Verspeek <l.verspeek@gmail.com>
2022-02-23 11:55:50 +0100

    add steps

--- components/Foot.vue
+++ components/Foot.vue
@@ -1,10 +1,10 @@
 <template>
   <footer class="footer container py-6 px-4">
     <div class="px-5 m-2 py-4 has-radius has-text-centered">
-      <img src="~/assets/img/Nosana_Logo_vertical_color_white.svg" class="logo" style="height: 80px">
+      <img src="~/assets/img/Nosana_Logo_vertical_color_black.svg" class="logo" style="height: 80px">
     </div>
     <div class="px-6 m-2 py-4 has-radius has-text-centered">
-      <small class="is-size-7"><span style="font-family: monospace">©</span> NOSANA 2021</small>
+      <small class="is-size-7"><span style="font-family: monospace">©</span> NOSANA 2022</small>
     </div>
   </footer>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,104 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title is-4">
-        Projects on <b class="has-text-accent">TestNet</b>
-      </h1>
+      <div class="columns">
+        <div class="column is-4">
+          <h1 class="title is-4">
+            Projects on <b class="has-text-accent">TestNet</b>
+          </h1>
+          <p class="has-limited-width-small">
+            Below you can find open-source repositories of projects that are running their pipelines on the Nosana TestNet
+          </p>
+        </div>
+        <div class="column is-8">
+          <div class="columns">
+            <div class="column is-one-third">
+              <a class="box is-secondary step" :class="{'has-background-white': loggedIn}" @click="$sol.loginModal = true">
+                <div class="is-flex is-justify-content-space-between">
+                  <div>1</div>
+                  <div v-if="loggedIn">
+                    <img :src="require('@/assets/img/icons/done.svg')">
+                  </div>
+                  <div v-else>
+                    <img :src="require('@/assets/img/icons/pending.svg')">
+                  </div>
+                </div>
+                <div class="has-text-centered my-2">
+                  <img src="~assets/img/icons/wallet.svg">
+                  <p>Connect Wallet</p>
+                </div>
+              </a>
+            </div>
+            <div class="column is-one-third">
+              <nuxt-link class="box is-secondary step" :class="{'has-background-white': user && repositories && repositories.filter(r => r.user_id === user.user_id).length, 'disabled': !loggedIn}" to="/repositories/new">
+                <div class="is-flex is-justify-content-space-between">
+                  <div>2</div>
+                  <div v-if="user && repositories && repositories.filter(r => r.user_id === user.user_id).length">
+                    <img :src="require('@/assets/img/icons/done.svg')">
+                  </div>
+                  <div v-else>
+                    <img :src="require('@/assets/img/icons/pending.svg')">
+                  </div>
+                </div>
+                <div class="has-text-centered my-2">
+                  <img v-if="loggedIn" src="~assets/img/icons/repository.svg">
+                  <img v-else src="~assets/img/icons/repository_grey.svg">
+                  <p>Add Repository</p>
+                </div>
+              </nuxt-link>
+            </div>
+            <div class="column is-one-third">
+              <nuxt-link class="box is-secondary step" :class="{'has-background-white': user && repositories && repositories.filter(r => r.user_id === user.user_id).length, 'disabled': !(loggedIn && user && repositories && repositories.filter(r => r.user_id === user.user_id).length)}" to="/account">
+                <div class="is-flex is-justify-content-space-between">
+                  <div>3</div>
+                  <div v-if="user && user.isApproved">
+                    <img :src="require('@/assets/img/icons/done.svg')">
+                  </div>
+                  <div v-else-if="user && user.name">
+                    <img :src="require('@/assets/img/icons/running.svg')">
+                  </div>
+                  <div v-else>
+                    <img :src="require('@/assets/img/icons/pending.svg')">
+                  </div>
+                </div>
+                <div class="has-text-centered my-2">
+                  <img v-if="loggedIn && user && repositories && repositories.filter(r => r.user_id === user.user_id).length" src="~assets/img/icons/project.svg">
+                  <img v-else src="~assets/img/icons/project_grey.svg">
+                  <p>Request Funds</p>
+                </div>
+              </nuxt-link>
+            </div>
+          </div>
+        </div>
+      </div>
       <nuxt-link v-if="loggedIn" to="/repositories/new" class="button is-accent is-outlined">
         Add new repository
       </nuxt-link>
       <div v-if="projects" class="columns is-multiline mt-4">
         <div v-for="project in projects" :key="project.id" class="column is-6 is-one-fifth-fullhd is-3-widescreen is-4-desktop">
-          <div class="box is-clickable" @click="$router.push('/projects/'+project.id)">
-            <div class="is-flex is-align-items-center">
+          <a class="box has-background-white is-clickable" @click="$router.push('/projects/'+project.id)">
+            <div class="is-flex is-align-items-flex-start is-justify-content-flex-start">
               <img style="height: 32px" :src="project.image" class="mr-4">
-              <h2 class="title">
-                {{ project.name }}
-              </h2>
+              <div>
+                <h2 class="title is-5 has-text-weight-semibold">
+                  {{ project.name }}
+                </h2>
+                <h2 class="subtitle is-6 mb-0">
+                  {{ project.name }}
+                </h2>
+                <p class="is-size-7">
+                  {{ project.description }}
+                </p>
+              </div>
             </div>
-            <p>
-              {{ project.description }}
-            </p>
-            <small>
-              Repositories:
+
+            <div class="mt-2">
               <span v-if="repositories">
-                {{ repositories.filter(r => r.user_id === project.id).length }}
                 <div v-if="!commits">
                   Loading..
                 </div>
                 <div v-else-if="!repositories.filter(r => r.user_id === project.id).map(r => r.commits).flat().length">
-                  no commits
+                  no pipelines
                 </div>
                 <div
                   v-else
@@ -46,8 +117,8 @@
                 </div>
               </span>
               <span v-else>Loading..</span>
-            </small>
-          </div>
+            </div>
+          </a>
         </div>
       </div>
     </div>
@@ -60,7 +131,8 @@ export default {
     return {
       repositories: null,
       commits: null,
-      projects: null
+      projects: null,
+      user: null
     }
   },
   computed: {
@@ -68,15 +140,40 @@ export default {
       return this.$sol && this.$sol.token
     }
   },
+  watch: {
+    '$sol.token' (token) {
+      if (token) {
+        this.getUser()
+      }
+    }
+  },
   created () {
     this.getRepositories()
     this.getProjects()
+    if (this.$sol && this.$sol.token) {
+      this.getUser()
+    }
     // setInterval(() => {
     //   console.log('refreshing repositories..')
     //   this.getRepositories()
     // }, 20000)
   },
   methods: {
+    async getUser () {
+      try {
+        const user = await this.$axios.$get(`${process.env.backendUrl}/user`)
+        this.name = user.name
+        this.description = user.description
+        this.image = user.image
+        this.user = user
+      } catch (error) {
+        this.$modal.show({
+          color: 'danger',
+          text: error,
+          title: 'Error'
+        })
+      }
+    },
     async getRepositories () {
       try {
         const repositories = await this.$axios.$get(`${process.env.backendUrl}/repositories`)
@@ -127,7 +224,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
- td {
-   vertical-align: middle;
+ .step {
+   min-height: 160px;
  }
 </style>
