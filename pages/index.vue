b759a881a7c60489404847b028048f44b98c3d0a
Laurens Verspeek <l.verspeek@gmail.com>
2022-03-02 15:33:48 +0100

    update text for each step

--- pages/index.vue
+++ pages/index.vue
@@ -79,11 +79,17 @@
           + Add new repository
         </nuxt-link>
       </div>
-      <p class="has-text-aligned is-horizontal-centered has-limited-width">
+      <p v-if="!loggedIn" class="has-text-aligned is-horizontal-centered has-limited-width">
+        Projects that want to run their pipelines on TestNet are being approved right now. Add your wallet, repositories and input your project information to request TestNet funds to be able to run jobs and earn NOS tokens for every pipeline that you are running! Selected projects will automatically receive 500 $NOS tokens for joining our Testnet. You can <b>earn up to 10.000 $NOS tokens</b> by running pipelines with our Testnet.
+      </p>
+      <p v-else-if="user && (!repositories || !repositories.filter(r => r.user_id === user.user_id).length)" class="has-text-aligned is-horizontal-centered has-limited-width">
         Projects that want to run their pipelines on TestNet are being approved right now. Add your repositories and input your project information to request TestNet funds to be able to run jobs and earn NOS tokens for every pipeline that you are running!
       </p>
-      <p v-if="user && user.name" class="is-horizontal-centered has-limited-width mt-4 notification is-info">
-        Your request for TestNet funds to run pipelines is pending. Make sure your information is correct and your repositories are added.
+      <p v-else-if="user && !user.name" class="has-text-aligned is-horizontal-centered has-limited-width">
+        Projects that want to run their pipelines on TestNet are being approved right now. Input your project information to request TestNet funds to be able to run jobs and earn NOS tokens for every pipeline that you are running!
+      </p>
+      <p v-else-if="user && user.name" class="is-horizontal-centered has-limited-width notification is-info">
+        Thank you for your submission. You can add/change repositories and project information during your pending request. We will inform you via email as soon as your project is approved. Approved projects can run jobs and earn NOS tokens for every pipeline!
       </p>
       <div v-if="repositories" class="columns is-multiline mt-4 has-background-secondary">
         <div v-if="!filteredRepositories.length" class="has-text-centered subtitle">
