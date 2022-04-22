46ed5bd47d34ea079e4f4574718e43d574a34d53
DaanPothof <daanpothof@me.com>
2022-04-22 16:01:49 +0200

    Change current color to Nosanna green

--- components/Pagination/PaginationBar.vue
+++ components/Pagination/PaginationBar.vue
@@ -18,13 +18,15 @@
         @click="nextPage"
       >Next page</a>
       <ul class="pagination-list">
-        <li><a class="pagination-link" aria-label="Goto page 1">1</a></li>
-        <li><span class="pagination-ellipsis">&hellip;</span></li>
-        <li><a class="pagination-link" aria-label="Goto page 45">45</a></li>
-        <li><a class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a></li>
-        <li><a class="pagination-link" aria-label="Goto page 47">47</a></li>
-        <li><span class="pagination-ellipsis">&hellip;</span></li>
-        <li><a class="pagination-link" aria-label="Goto page 86">86</a></li>
+        <li v-for="(page, index) in pages" :key="page">
+          <a
+            class="pagination-link"
+            :class="{'is-current': currentPage === index + 1}"
+            aria-label="Goto page 1"
+            arie-current="page"
+          >{{ index + 1 }}</a>
+        </li>
+        <!-- <li><span class="pagination-ellipsis">&hellip;</span></li> -->
       </ul>
     </nav>
     <p>CurrentPage: {{ currentPage }}</p>
@@ -34,6 +36,14 @@
 
 <script>
 export default {
+  props: {
+    pages: {
+      type: Object,
+      default () {
+        return {};
+      }
+    }
+  },
   data () {
     return {
       currentPage: 1,
