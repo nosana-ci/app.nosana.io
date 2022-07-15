b9fa3fe3388841e7b16d22f9b51d40d8036a7d82
Laurens Verspeek <l.verspeek@gmail.com>
2022-07-15 18:08:24 +0200

    show stake with disconnected wallet

--- pages/stake.vue
+++ pages/stake.vue
@@ -4,20 +4,34 @@
       <h1 class="title is-spaced">
         Stake
       </h1>
-      <div v-if="$sol && $sol.publicKey">
+      <div>
         <div v-if="stakeData === null">
           Loading..
         </div>
-        <h2 v-else-if="stakeData" class="subtitle">
-          Staked <b>{{ stakeData.amount/1e9 }} NOS</b> for
-          <b>{{ $moment.duration(stakeData.duration, 'seconds').humanize() }}</b>
-        </h2>
-        <div v-if="balance === null">
-          Loading..
+        <div v-else-if="stakeData">
+          <div>
+            <span v-if="!amount">Current</span><span v-else>New</span> Stake
+          </div>
+          <h2 class="subtitle has-text-weight-bold">
+            <ICountUp
+              :end-val="amount ? stakeData.amount/1e9 + parseFloat(amount) : stakeData.amount/1e9"
+              :options="{ decimalPlaces: 2 }"
+            /> <small class="is-size-6">NOS</small>
+          </h2>
+          <div>
+            Unstake duration
+          </div>
+          <h2 class="subtitle has-text-weight-bold">
+            {{ $moment.duration(stakeData.duration, 'seconds').humanize() }}
+          </h2>
         </div>
-        <div v-else>
-          Available Balance: {{ balance }} NOS
-          <a v-if="balance === 0" href="https://nosana.io/token" target="_blank">Buy NOS tokens</a>
+        <br>
+        <div class="is-pulled-right">
+          <span v-if="balance === null">Loading..</span>
+          <span v-else>
+            Balance: <a @click="amount = balance">{{ balance }} NOS</a>
+            <a v-if="balance === 0" href="https://nosana.io/token" target="_blank">Buy NOS tokens</a>
+          </span>
         </div>
       </div>
       <form @submit.prevent="stake">
@@ -38,14 +52,25 @@
         <div v-if="!stakeData" class="field">
           <label class="label">Unstake Days</label>
           <div class="control">
-            <input v-model="unstakeDays" required class="input" type="number" placeholder="0 days">
+            <input
+              v-model="unstakeDays"
+              required
+              class="input"
+              type="number"
+              :min="31"
+              :max="365"
+              placeholder="0 days"
+            >
           </div>
         </div>
-        <h2 class="subtitle">
-          {{ xNOS }} xNOS
+        <div>
+          <span v-if="!amount">Current</span><span v-else>New</span> xNOS score
+        </div>
+        <h2 class="title">
+          <ICountUp :end-val="xNOS" :options="{ decimalPlaces: 2 }" /> <small class="is-size-5">xNOS</small>
         </h2>
         <button
-          v-if="!$sol || !$sol.publicKey"
+          v-if="!loggedIn"
           class="button is-accent is-outlined has-text-weight-semibold"
           @click.stop.prevent="$sol.loginModal = true"
         >
@@ -62,6 +87,7 @@
   </section>
 </template>
 <script>
+import ICountUp from 'vue-countup-v2';
 const anchor = require('@project-serum/anchor');
 const TOKEN_PROGRAM_ID = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA';
 
@@ -70,11 +96,25 @@ const idl = require('@/utils/nosana_staking.json');
 const ENV = process.env.NUXT_ENV_SOL_NETWORK;
 const SECONDS_PER_DAY = 24 * 60 * 60;
 
+class FakeWallet {
+  constructor (payer) {
+    this.payer = payer;
+  }
+
+  get publicKey () {
+    return this.payer.publicKey;
+  }
+}
+
 let node = ENV;
 if (!node.includes('http')) {
   node = anchor.web3.clusterApiUrl(ENV);
 }
+const web3 = new anchor.web3.Connection(node, 'confirmed');
 export default {
+  components: {
+    ICountUp
+  },
   middleware: 'auth',
   data () {
     return {
@@ -90,6 +130,9 @@ export default {
     };
   },
   computed: {
+    loggedIn () {
+      return this.$sol && this.$sol.publicKey;
+    },
     xNOS () {
       if (!this.unstakeDays && !this.stakeData) {
         return 0;
@@ -121,24 +164,25 @@ export default {
   },
   mounted () {
     console.log('initializing anchor client');
-    const wallet = this.$sol.getWallet();
-    if (wallet) {
-      this.initAnchor(wallet);
+    let wallet = this.$sol.getWallet();
+    if (!wallet) {
+      wallet = new FakeWallet(anchor.web3.Keypair.generate());
     }
+    this.initAnchor(wallet);
   },
   methods: {
     async initAnchor (wallet) {
-      this.provider = new anchor.AnchorProvider(this.$sol.web3, wallet, {});
-
+      this.provider = new anchor.AnchorProvider(web3, wallet, {});
+      const userKey = new anchor.web3.PublicKey(this.$auth.user.address);
       const accounts = {
         // solana native
         systemProgram: anchor.web3.SystemProgram.programId,
         rent: anchor.web3.SYSVAR_RENT_PUBKEY,
         clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
         tokenProgram: new anchor.web3.PublicKey(TOKEN_PROGRAM_ID),
-        feePayer: this.provider.wallet.publicKey,
         // custom
-        authority: this.provider.wallet.publicKey,
+        feePayer: userKey,
+        authority: userKey,
         ataVault: undefined,
         mint: new anchor.web3.PublicKey(process.env.NUXT_ENV_NOS_TOKEN),
         ataFrom: undefined,
@@ -152,12 +196,12 @@ export default {
       accounts.ataVault = ataVault;
       console.log(ataVault.toString(), bump);
       [accounts.stake] = await anchor.web3.PublicKey.findProgramAddress(
-        [anchor.utils.bytes.utf8.encode('stake'), this.provider.wallet.publicKey.toBuffer()],
+        [anchor.utils.bytes.utf8.encode('stake'), userKey.toBuffer()],
         this.program.programId
       );
       accounts.ataFrom = (await anchor.web3.PublicKey.findProgramAddress(
         [
-          accounts.authority.toBuffer(),
+          userKey.toBuffer(),
           accounts.tokenProgram.toBuffer(),
           accounts.mint.toBuffer()
         ],
@@ -180,17 +224,14 @@ export default {
       this.getBalance();
     },
     async getBalance () {
-      const account = await this.$sol.web3.getTokenAccountsByOwner(this.accounts.authority,
+      const account = await web3.getTokenAccountsByOwner(this.accounts.authority,
         { mint: this.accounts.mint });
       if (account && account.value && account.value.length > 0) {
         const tokenAddress = new anchor.web3.PublicKey(account.value[0].pubkey.toString());
 
-        this.balance = (await this.$sol.web3.getTokenAccountBalance(
+        this.balance = (await web3.getTokenAccountBalance(
           tokenAddress
         )).value.uiAmount;
-        if (this.amount === null) {
-          this.amount = this.balance;
-        }
       } else {
         this.balance = 0;
       }
