89d8cadfaf79466e76603afe33d52f7d0b380c60
Laurens Verspeek <l.verspeek@gmail.com>
2022-08-11 11:44:08 +0200

    stretch out stats page

--- pages/stats.vue
+++ pages/stats.vue
@@ -1,41 +1,39 @@
 <template>
   <section class="section">
-    <div class="container">
-      <div class="columns">
-        <div class="column is-4">
-          <h1 class="title is-4">
-            Statistics for <b v-if="network === 'devnet'" class="has-text-accent">DevNet</b>
-            <b v-else class="has-text-accent">TestNet</b>
-          </h1>
-          <p class="has-limited-width-small">
-            Below you can find live statistics about the number of commits, jobs, users and repositories
-            on the Nosana TestNet
-          </p>
-        </div>
+    <div class="columns">
+      <div class="column is-4">
+        <h1 class="title is-4">
+          Statistics for <b v-if="network === 'devnet'" class="has-text-accent">DevNet</b>
+          <b v-else class="has-text-accent">TestNet</b>
+        </h1>
+        <p class="has-limited-width-small">
+          Below you can find live statistics about the number of commits, jobs, users and repositories
+          on the Nosana TestNet
+        </p>
       </div>
-      <div v-if="stats" class="columns is-multiline is-mobile">
-        <div
-          v-for="(value, stat) in stats"
-          :key="stat"
-          class="column is-2-widescreen is-one-fifth-desktop is-3-tablet is-6-mobile"
-        >
-          <div class="box has-text-centered">
-            <div class="is-size-7">
-              {{ stat.split("_").map((w) => (w[0].toUpperCase() + w.substring(1))).join(" ") }}
-            </div>
-            <h2
-              class="title is-4"
-              :class="{
-                'has-text-info': stat.includes('running'),
-                'has-text-danger': stat.includes('failed'),
-                'has-text-warning': stat.includes('queued'),
-                'has-text-success': stat.includes('success'),
-                'has-text-accent': stat.includes('reward')
-              }"
-            >
-              <ICountUp :end-val="value" /> <small v-if="stat.includes('reward')" class="is-size-6">NOS</small>
-            </h2>
+    </div>
+    <div v-if="stats" class="columns is-multiline is-mobile">
+      <div
+        v-for="(value, stat) in stats"
+        :key="stat"
+        class="column is-2-widescreen is-one-fifth-desktop is-3-tablet is-6-mobile"
+      >
+        <div class="box has-text-centered">
+          <div class="is-size-7">
+            {{ stat.split("_").map((w) => (w[0].toUpperCase() + w.substring(1))).join(" ") }}
           </div>
+          <h2
+            class="title is-4"
+            :class="{
+              'has-text-info': stat.includes('running'),
+              'has-text-danger': stat.includes('failed'),
+              'has-text-warning': stat.includes('queued'),
+              'has-text-success': stat.includes('success'),
+              'has-text-accent': stat.includes('reward')
+            }"
+          >
+            <ICountUp :end-val="value" /> <small v-if="stat.includes('reward')" class="is-size-6">NOS</small>
+          </h2>
         </div>
       </div>
     </div>
