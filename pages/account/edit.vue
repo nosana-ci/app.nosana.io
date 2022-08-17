0760c967986be3f6699a3cfa0ebf11a55a316810
djmbritt <david@effect.ai>
2022-08-17 15:57:47 +0200

    Create profile page.

---
+++ pages/account/edit.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="title">
+      Edit User Profile
+    </div>
+
+    <form @submit.prevent="updateUser">
+      <section class="modal-card-body">
+        <div class="field">
+          <label>Name*:</label>
+          <input v-model="name" required type="text" class="input">
+        </div>
+        <div class="field">
+          <label>Email*:</label>
+          <input v-model="email" required type="email" class="input">
+        </div>
+        <div class="field">
+          <label>Description:</label>
+          <textarea v-model="description" class="textarea" />
+        </div>
+        <div class="field">
+          <label>Icon URL:</label>
+          <input v-model="image" type="url" class="input">
+        </div>
+        <div class="field">
+          <label>Discord Username:</label>
+          <input v-model="discord" type="text" class="input">
+        </div>
+      </section>
+
+      <footer class="modal-card-foot has-text-right">
+        <button class="button" @click.prevent="editUser = false; removeQuery()">
+          Cancel
+        </button>
+        <input type="submit" class="button is-accent" value="Save">
+      </footer>
+    </form>
+    <button class="modal-close is-large" aria-label="close" @click="editUser = false; removeQuery()" />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  middleware: 'auth',
+  data () {
+    return {
+      user: null,
+      image: null,
+      description: null,
+      discord: null,
+      email: null,
+      name: null,
+      editUser: false,
+      repositories: null,
+      commits: null,
+      balance: null,
+      usedBalance: null
+    };
+  },
+  computed: {
+
+  },
+  created () {
+
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style>
+
+</style>
--- pages/account/index.vue
+++ pages/account/index.vue
@@ -140,48 +140,6 @@
           </template>
         </div>
       </div>
-      <div class="modal" :class="{'is-active': editUser}">
-        <div class="modal-background" @click="editUser = false; removeQuery()" />
-        <div class="modal-card">
-          <header class="modal-card-head">
-            <p class="modal-card-title">
-              Register Project
-            </p>
-          </header>
-          <form @submit.prevent="updateUser">
-            <section class="modal-card-body">
-              <div class="field">
-                <label>Name*:</label>
-                <input v-model="name" required type="text" class="input">
-              </div>
-              <div class="field">
-                <label>Email*:</label>
-                <input v-model="email" required type="email" class="input">
-              </div>
-              <div class="field">
-                <label>Description:</label>
-                <textarea v-model="description" class="textarea" />
-              </div>
-              <div class="field">
-                <label>Icon URL:</label>
-                <input v-model="image" type="url" class="input">
-              </div>
-              <div class="field">
-                <label>Discord Username:</label>
-                <input v-model="discord" type="text" class="input">
-              </div>
-            </section>
-
-            <footer class="modal-card-foot has-text-right">
-              <button class="button" @click.prevent="editUser = false; removeQuery()">
-                Cancel
-              </button>
-              <input type="submit" class="button is-accent" value="Save">
-            </footer>
-          </form>
-        </div>
-        <button class="modal-close is-large" aria-label="close" @click="editUser = false; removeQuery()" />
-      </div>
     </div>
     <div v-if="user" class="mt-6">
       <nuxt-link to="/repositories/new" class="button is-accent is-outlined is-pulled-right">
@@ -200,7 +158,7 @@
 </template>
 
 <script>
-import RepositoryList from '../components/RepositoryList.vue';
+import RepositoryList from '@/components/RepositoryList.vue';
 
 export default {
   components: { RepositoryList },
