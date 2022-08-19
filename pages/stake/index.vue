1a32da6217863efa046683c4bbc837d4707c3f7d
Laurens Verspeek <l.verspeek@gmail.com>
2022-08-19 16:37:31 +0200

    design improvements

--- pages/stake/index.vue
+++ pages/stake/index.vue
@@ -8,9 +8,10 @@
     </h1>
     <p>Stake NOS and generate $NOS and $xNOS</p>
     <div class="tile is-ancestor mt-6">
-      <div class="tile is-vertical is-parent">
+      <div class="tile is-vertical is-parent" style="max-width: 650px">
         <staking-form
           class="tile is-child"
+          style="flex-grow: 0"
           :stake-data="stakeData"
           :stake-end-date="stakeEndDate"
           @x-nos="updateXNOS"
