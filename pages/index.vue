872a734209a393467d4a0c69f2cd5764d851aef6
Laurens Verspeek <l.verspeek@gmail.com>
2023-01-23 17:08:07 +0100

    WIP: job page rework

--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,9 @@
     <div>
       <div v-if="!loggedIn">
         <h1 class="title has-text-centered">
-          Register
+          Loading..
         </h1>
-        <img class="image mx-auto" src="https://nosana.io/img/Nosana_Logomark_black.svg" alt="" srcset="" style="max-height: 400px;">
-        <br>
-        <div class="buttons is-centered p-6 mx-6">
-          <div
-            class="button is-accent is-large has-text-weight-semibold"
-            exact-active-class="is-active"
-            @click="$sol.loginModal = true"
-          >
-            Connect Wallet
-          </div>
-          <div
-            class="button is-accent is-large has-text-weight-semibold"
-            exact-active-class="is-active"
-            @click="goToGithub"
-          >
-            Login with Github
-          </div>
-        </div>
+        <img class="image mx-auto" src="https://nosana.io/img/Nosana_Logomark_black.svg" alt="" srcset="" style="max-height: 200px;">
       </div>
     </div>
   </section>
@@ -55,9 +38,6 @@ export default {
     }
   },
   methods: {
-    goToGithub () {
-      window.location.href = `https://github.com/login/oauth/authorize?client_id=${process.env.NUXT_ENV_GITHUB_APP_CLIENT_ID}&redirect_uri=${window.location.origin}/login`;
-    }
   }
 };
 
